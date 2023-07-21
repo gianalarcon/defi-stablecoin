9f91ec0cfc492b6885eb6af193b646b780b542bd
gianmarcoalarcon <gian.alarcon@unmsm.edu.pe>
2023-07-21 20:48:53 +0800

    Feat: Add deploy scripts:  DeployDSC and HelperConfig

---
+++ script/HelperConfig.s.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+
+contract HelperConfig is Script {
+    struct NetworkConfig {
+        address wethUsdPriceFeed;
+        address wbtcUsdPriceFeed;
+        address weth;
+        address wbtc;
+        uint256 deployKey;
+    }
+
+    NetworkConfig public activeNetworking;
+
+    constructor() {
+        if (block.chainid == 11155111) {
+            activeNetworking = getSepoliaEthConfig();
+        }
+    }
+
+    function getSepoliaEthConfig() public view returns (NetworkConfig memory) {
+        return NetworkConfig({
+            wethUsdPriceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306,
+            wbtcUsdPriceFeed: 0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43,
+            weth: 0xdd13E55209Fd76AfE204dBda4007C227904f0a81,
+            wbtc: 0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063,
+            deployKey: vm.envUint("PRIVATE_KEY")
+        });
+    }
+}
